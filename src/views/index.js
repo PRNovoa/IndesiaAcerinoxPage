882d4a006c518d6ea132b2540aaf0796c1b8e235
Pablo Novoa <67236009+PRNovoa@users.noreply.github.com>
2025-11-20 20:42:47 +0100

    Initial project setup with core files
    Add initial project structure including .gitignore, index.html, configuration files, source code, components, views, assets, and package management files for the Acerinox licitaciones system.

---
+++ src/views/index.js
@@ -0,0 +1,7 @@
+// demo/src/views/index.js
+export const views = {
+    '/': { templateId: 'view-filtros', templateUrl: './src/views/filtros.html' },
+    '/licitaciones': { templateId: 'view-licitaciones', templateUrl: './src/views/licitaciones.html' },
+    '/detalle-licitacion': { templateId: 'view-detalle-licitacion', templateUrl: './src/views/detalle-licitacion.html' },
+    404: { templateId: 'view-filtros', templateUrl: './src/views/filtros.html' },
+};
